51e09bb1095a8cffe9b34b70cd3b53f148a6496d
Grzesiek <grzesieksiwy2000@gmail.com>
2026-01-17 20:26:27 +0100

    server.js

--- backend/server.js
+++ backend/server.js
@@ -6,28 +6,39 @@ import authRoutes from "./routes/auth.js";
 import entriesRoutes from "./routes/entries.js";
 
 dotenv.config();
+
 const app = express();
 
-// middelwares
-app.use(cors());
-app.use(express.json()); 
 
-// connect to Mongodb
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB connect error:", err));
+app.use(cors({
+  origin: "https://travel-journal22.netlify.app",
+  credentials: true
+}));
+app.use(express.json());
 
-// routes
+
+app.get("/", (req, res) => res.json({ ok: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/entries", entriesRoutes);
 
-// health check
-app.get("/", (req, res) => res.json({ ok: true }));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
 
 app.use((err, req, res, next) => {
-  console.error("GLOBAL ERROR:", err);
+  console.error("🔥 GLOBAL ERROR:", err);
   res.status(500).json({ message: "Internal Server Error" });
 });
+
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log("Server running on port", PORT);
+    });
+  })
+  .catch(err => {
+    console.error("MongoDB connection failed:", err);
+    process.exit(1); 
+  });
