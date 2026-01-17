18df101ea911ed22057870f8c94433f18cbce90b
Grzesiek <grzesieksiwy2000@gmail.com>
2026-01-17 19:18:36 +0100

    upload limit deleted

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -2,20 +2,6 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-const upload = multer({
-  storage,
-
-  limits: {
-    fileSize: 5 * 1024 * 1024, // limit 5 MB na plik
-  },
-
-  fileFilter(req, file, cb) {
-    if (!file.mimetype.startsWith("image/")) {
-      cb(new Error("Only image files are allowed"), false);
-      return;
-    }
-    cb(null, true);
-  }
-});
+const upload = multer({ storage });
 
 export default upload;
